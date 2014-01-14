8ea754c7f6855ec7bd45d0c8731a6aef234cf4a7
Matthias Bolte <matthias@tinkerforge.com>
2014-01-14 15:34:35 +0100

    perl: Fix warning about unsharing the $tilt variable

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -5,10 +5,11 @@ use Tinkerforge::BrickletTilt;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
+use constant UID => 'gfdd'; # Change to your UID
 use constant UID => 'fgfds'; # Change to your UID
 
 my $ipcon = IPConnection->new(); # Create IP connection
-my $tilt = BrickletTilt->new(&UID, $ipcon); # Create device object
+our $tilt = BrickletTilt->new(&UID, $ipcon); # Create device object
 
 # Callback function for tilt state callback
 sub cb_tilt_state
@@ -19,11 +20,12 @@ sub cb_tilt_state
     {
         print "\nclosed\n";
     }
-        
+
     if ($tilt_state == $tilt->TILT_STATE_OPEN)
     {
         print "\nopen\n";
     }
+
     if ($tilt_state == $tilt->TILT_STATE_CLOSED_VIBRATING)
     {
         print "\nclosed vibrating\n";
