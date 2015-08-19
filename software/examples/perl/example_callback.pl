c012d2753d966c0ed16cb3c22fdcc6b35f8ed851
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 16:18:21 +0200

    Cleanup Perl examples

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -1,29 +1,29 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletTilt;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'fgfds'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-our $tilt = Tinkerforge::BrickletTilt->new(&UID, $ipcon); # Create device object
+our $t = Tinkerforge::BrickletTilt->new(&UID, $ipcon); # Create device object
 
 # Callback function for tilt state callback
 sub cb_tilt_state
 {
     my ($tilt_state) = @_;
 
-    if ($tilt_state == $tilt->TILT_STATE_CLOSED)
+    if ($tilt_state == $t->TILT_STATE_CLOSED)
     {
         print "closed\n";
     }
-    elsif ($tilt_state == $tilt->TILT_STATE_OPEN)
+    elsif ($tilt_state == $t->TILT_STATE_OPEN)
     {
         print "open\n";
     }
-    elsif ($tilt_state == $tilt->TILT_STATE_CLOSED_VIBRATING)
+    elsif ($tilt_state == $t->TILT_STATE_CLOSED_VIBRATING)
     {
         print "closed vibrating\n";
     }
@@ -33,12 +33,11 @@ $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Enable tilt state callback
-$tilt->enable_tilt_state_callback();
+$t->enable_tilt_state_callback();
 
 # Register tilt state callback to function cb_tilt_state
-$tilt->register_callback($tilt->CALLBACK_TILT_STATE, 'cb_tilt_state');
+$t->register_callback($t->CALLBACK_TILT_STATE, 'cb_tilt_state');
 
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -1,30 +1,30 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletTilt;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'fgfds'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $tilt = Tinkerforge::BrickletTilt->new(&UID, $ipcon); # Create device object
+my $t = Tinkerforge::BrickletTilt->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Get current tilt state
-my $tilt_state = $tilt->get_tilt_state();
+my $tilt_state = $t->get_tilt_state();
 
-if ($tilt_state == $tilt->TILT_STATE_CLOSED)
+if ($tilt_state == $t->TILT_STATE_CLOSED)
 {
     print "closed\n";
 }
-elsif ($tilt_state == $tilt->TILT_STATE_OPEN)
+elsif ($tilt_state == $t->TILT_STATE_OPEN)
 {
     print "open\n";
 }
-elsif ($tilt_state == $tilt->TILT_STATE_CLOSED_VIBRATING)
+elsif ($tilt_state == $t->TILT_STATE_CLOSED_VIBRATING)
 {
     print "closed vibrating\n";
 }
@@ -32,5 +32,3 @@ elsif ($tilt_state == $tilt->TILT_STATE_CLOSED_VIBRATING)
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
-
