c1033bb683ae77a5e17874d77858ce79542c32cd
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:16:59 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/tilt_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Tilt Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	t, _ := tilt_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Enable tilt state callback
+	t.EnableTiltStateCallback()
+
+	t.RegisterTiltStateCallback(func(state tilt_bricklet.TiltState) {
+
+		if state == tilt_bricklet.TiltStateClosed {
+			fmt.Println("Tilt State: Closed")
+		} else if state == tilt_bricklet.TiltStateOpen {
+			fmt.Println("Tilt State: Open")
+		} else if state == tilt_bricklet.TiltStateClosedVibrating {
+			fmt.Println("Tilt State: Closed Vibrating")
+		}
+	})
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/tilt_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Tilt Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	t, _ := tilt_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Get current tilt state.
+	state, _ := t.GetTiltState()
+
+	if state == tilt_bricklet.TiltStateClosed {
+		fmt.Println("Tilt State: Closed")
+	} else if state == tilt_bricklet.TiltStateOpen {
+		fmt.Println("Tilt State: Open")
+	} else if state == tilt_bricklet.TiltStateClosedVibrating {
+		fmt.Println("Tilt State: Closed Vibrating")
+	}
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
